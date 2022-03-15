05e40ab3fbc0fa083ebfebf417755ec99c781be7
Skronkie <ebrockway01@gmail.com>
2022-03-15 18:42:44 -0500

    added delete route/query for a specific column's id

--- server.js
+++ server.js
@@ -16,32 +16,64 @@ const db = mysql.createConnection(
   },
   console.log("Connected to the election database!")
 );
+app.get("/api/candidates", (req, res) => {
+  const sql = `SELECT * FROM candidates`;
 
-// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(row);
-// });
+  db.query(sql, (err, row) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: row,
+    });
+  });
+});
 
-// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+app.get("/api/candidate/:id", (req, res) => {
+  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({ message: "success", data: row });
+  });
+});
+app.delete("/api/candidate/:id", (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.statusMessage(500).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({ message: "Candidate not found!" });
+    } else {
+      res.json({
+        message: "deleted",
+        changes: result.affectedRows,
+        id: req.params.id,
+      });
+    }
+    console.log(result);
+  });
+});
+
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+// VALUES (?,?,?,?)`;
+
+// const params = [1, "Ronald", "Firbank", 1];
+
+// db.query(sql, params, (err, result) => {
 //   if (err) {
 //     console.log(err);
 //   }
 //   console.log(result);
 // });
-
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-VALUES (?,?,?,?)`;
-
-const params = [1, "Ronald", "Firbank", 1];
-
-db.query(sql, params, (err, result) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log(result);
-});
 
 app.use((req, res) => {
   res.status(404).end();
