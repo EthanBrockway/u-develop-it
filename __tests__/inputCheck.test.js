dee813c6ecdcf692ba3b62618cc85a803626a5ea
Skronkie <ebrockway01@gmail.com>
2022-03-15 13:51:17 -0500

    installed packages

--- __tests__/inputCheck.test.js
+++ __tests__/inputCheck.test.js
@@ -1,12 +1,12 @@
 const inputCheck = require("../utils/inputCheck");
 
-test("inputCheck() returns null when all properties exist", () => {
+it("inputCheck() returns null when all properties exist", () => {
   const obj = { name: "alice" };
 
   expect(inputCheck(obj, "name")).toBe(null);
 });
 
-test("inputCheck() returns an object when a property is missing", () => {
+it("inputCheck() returns an object when a property is missing", () => {
   const obj = { name: "alice", occupation: "" };
 
   expect(inputCheck(obj, "name", "occupation")).toEqual(
